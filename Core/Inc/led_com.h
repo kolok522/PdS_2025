c1a5070e8f887e8b6bb593db0e5db001ad9ce190
kolok522 <dacusin99@gmail.com>
2025-05-13 20:00:12 +0200

    PdS_2025

---
+++ Core/Inc/led_com.h
@@ -0,0 +1,16 @@
+/*
+ * led_com.h
+ *
+ *  Created on: May 6, 2025
+ *      Author: dacus
+ */
+
+#ifndef INC_LED_COM_H_
+#define INC_LED_COM_H_
+
+void led_init(void);
+void ON(void);
+void OFF(void);
+void TOGGLE(void);
+
+#endif /* INC_LED_COM_H_ */
---
+++ Core/Inc/main_user.h
@@ -0,0 +1,13 @@
+/*
+ * main_user.h
+ *
+ *  Created on: May 6, 2025
+ *      Author: dacus
+ */
+
+#ifndef INC_MAIN_USER_H_
+#define INC_MAIN_USER_H_
+
+void main_user(void);
+
+#endif /* INC_MAIN_USER_H_ */
